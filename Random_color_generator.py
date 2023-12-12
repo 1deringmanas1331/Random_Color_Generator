e186272d5333b25a5c62fe2301c32c5ddd9aae20
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-12-12 07:39:03 +0530

    Random_color_generator.py

--- Random_color_generator.py
+++ Random_color_generator.py
@@ -1,7 +1,7 @@
 # Import random library
 import random
 
-#Define a function for generating a random color
+# Define a function for generating a random color
 def generate_random_color():
     # Generate any random color code with this logic
     red = random.randint(0, 255)
